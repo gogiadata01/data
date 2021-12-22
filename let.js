06245642f83ef2e89c57cb63a76601e51b1458e0
gogiadata01 <94321560+gogiadata01@users.noreply.github.com>
2021-12-22 19:43:58 +0400

    Add files via upload

---
+++ let.js
@@ -0,0 +1,88 @@
+//  var mydogname = "grut";
+
+//  var mydogname="hachi";
+
+//  console.log(mydogname)
+
+
+
+//  let i = 6;
+
+// const s = "10";
+
+//  i++;
+
+//  document.body.innerHTML += i;
+//  document.body.innerHTML +=s;
+
+//  console.log(i + s)
+
+
+//  let numb = 115;
+//  let array = [50, 'lomi', 'hi', 100, 155];
+//  let string = 'you are awesome';
+//  let obj ={color: 'red', car:maxspeed='150km/h'}
+// let bool = true;
+//  let bool1 =false;
+
+
+//  console.log(array)
+//  console.log(string)
+//  console.log(115 == 115);
+//  console.log(numb);
+//  console.log(typeof(bool1))
+//   document.getElementById("text").innerHTML = "valhalla calling "
+
+//   let year = 10
+  
+  
+  
+//  let url ="https://getbootstrap.com/docs/5.1/components/card/";
+//  let img = "img/banana.png";
+//  let mystring ='div class="col-lg-4"><div class="card">
+//  <img src="'+img+'" class="card-img-top" alt="...">
+//  <div class="card-body">
+//    <h5 class="card-title">Card title</h5>
+//    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//    <a href="'+url+'" class="btn btn-primary">Go somewhere</a>
+//  </div>'
+//  </div>'
+//  </div>';
+ 
+//  document.getElementById("output").innerHTML = mystring
+
+
+let momxmarebeli ={
+  name: "data",
+  email: "datagogia777@gmail.com",
+  Number: "555555556555",
+  role: 1,
+
+  checkadmin:function(){
+    if(this.role==1){
+      return true
+    }
+    return false
+  }
+}
+
+momxmarebeli.role = 3,
+
+console.log(momxmarebeli)
+
+let key = Object.keys(momxmarebeli);
+let val = Object.values(momxmarebeli);
+let all = Object.entries(momxmarebeli);
+
+console.log(key, val);
+console.log(momxmarebeli.role);
+
+boxs.innerhtml = '<div class="card" >
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>'
+
